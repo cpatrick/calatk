5eb372bc4e9964a2cd9d91001e8a7eba4a3acee8
Matt McCormick <matt.mccormick@kitware.com>
2012-02-17 15:36:59 -0500

    Fix how the valgrind memory suppressions are handled.
    In a hard-learned lesson on CTest foo, the valgrind suppressions file cannot and
    should not be set in CTestCustom.cmake.  Instead, for dashboard submissions such
    as
    
      ctest -D ExperimentalMemCheck
    
    It should be set in the CMakeCache.txt with the variable
    MEMORYCHECK_SUPPRESSIONS_FILE.
    
    For CTest script submissions, such as for the nightly dashboard, it should be
    set in the dashboard script with CTEST_MEMORYCHECK_SUPPRESSIONS_FILE.

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -5,9 +5,3 @@ set(CTEST_DROP_METHOD "http")
 set(CTEST_DROP_SITE "www.cdash.org")
 set(CTEST_DROP_LOCATION "/CDash/submit.php?project=calaTK")
 set(CTEST_DROP_SITE_CDASH TRUE)
-
-message( "${CMAKE_CURRENT_SOURCE_DIR} hi mom" )
-set(CTEST_MEMORYCHECK_SUPPRESSIONS_FILE
-  "${CMAKE_CURRENT_SOURCE_DIR}/Scripts/Valgrind/CALATKValgrind.supp"
-  )
-
