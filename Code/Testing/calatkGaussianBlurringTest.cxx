a24e5851e0f2137894ebdbb064a89d058baeef4b
Matt McCormick <matt.mccormick@kitware.com>
2012-03-20 17:06:13 -0400

    Fix some memory leaks in calatkGaussianBlurringTest.
    Change-Id: Ie04b9c66ba5f76c1606cce6f8510cf89a35a944b

--- Code/Testing/calatkGaussianBlurringTest.cxx
+++ Code/Testing/calatkGaussianBlurringTest.cxx
@@ -38,20 +38,20 @@ int calatkGaussianBlurringTest( int argc, char* argv[] )
     return EXIT_FAILURE;
     }
 
-  std::cout << "Input: " << argv[1] << std::endl;
+  std::cout << "Input:  " << argv[1] << std::endl;
   std::cout << "Output: " << argv[2] << std::endl;
 
-  typedef CALATK::VectorImage< TFLOAT, DIMENSION > VectorImageType;
+  typedef CALATK::VectorImage< TFLOAT, DIMENSION >      VectorImageType;
   typedef CALATK::VectorImageUtils< TFLOAT, DIMENSION > VectorImageUtilsType;
 
-  VectorImageType* pIm = VectorImageUtilsType::readFileITK( argv[1] );
-  VectorImageType* pImBlurred = new VectorImageType( pIm );
+  typename VectorImageType::Pointer pIm = VectorImageUtilsType::readFileITK( argv[1] );
+  typename VectorImageType::Pointer pImBlurred = new VectorImageType( pIm );
 
   typedef CALATK::CGaussianKernel< TFLOAT, DIMENSION > GaussianKernelType;
-  GaussianKernelType gaussianKernel;
+  typename GaussianKernelType::Pointer gaussianKernel = new GaussianKernelType;
 
-  gaussianKernel.SetSigma( 0.1 );
-  gaussianKernel.ConvolveWithKernel( pImBlurred );
+  gaussianKernel->SetSigma( 0.1 );
+  gaussianKernel->ConvolveWithKernel( pImBlurred );
 
   VectorImageUtilsType::writeFileITK( pImBlurred, argv[2] );
 
