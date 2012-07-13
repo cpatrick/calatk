e366154220947c40063ee526219c72bc5b1f2712
guillaumesala <guillaume.sala@kitware.com>
2012-07-13 13:21:53 -0400

    Superbuild for building ITK

---
+++ SuperBuild.cmake
@@ -0,0 +1,87 @@
+include( ExternalProject )
+
+set( base "${CMAKE_BINARY_DIR}" )
+set_property( DIRECTORY PROPERTY EP_BASE ${base} )
+
+set( shared ON )
+set( testing OFF )
+set( build_type "Debug" )
+if( CMAKE_BUILD_TYPE )
+  set( build_type "${CMAKE_BUILD_TYPE}" )
+endif()
+
+set( CALATK_DEPENDS "" )
+
+set( gen "${CMAKE_GENERATOR}" )
+
+##
+## Check if sytem ITK or superbuild ITK
+##
+if( NOT USE_SYSTEM_ITK )
+
+  if( NOT GIT_EXECUTABLE )
+    find_package( Git REQUIRED )
+  endif( NOT GIT_EXECUTABLE )
+
+  option( GIT_PROTOCOL_HTTP
+    "Use HTTP for git access (useful if behind a firewall)" OFF )
+  if( GIT_PROTOCOL_HTTP )
+    set( GIT_PROTOCOL "http" CACHE STRING "Git protocol for file transfer" )
+  else( GIT_PROTOCOL_HTTP )
+    set( GIT_PROTOCOL "git" CACHE STRING "Git protocol for file transfer" )
+  endif( GIT_PROTOCOL_HTTP )
+  mark_as_advanced( GIT_PROTOCOL )
+
+  ##
+  ## Insight
+  ##
+  set( proj Insight )
+  ExternalProject_Add( ${proj}
+    GIT_REPOSITORY "${GIT_PROTOCOL}://itk.org/ITK.git"
+    GIT_TAG "origin/master"
+    SOURCE_DIR "${CMAKE_BINARY_DIR}/Insight"
+    BINARY_DIR Insight-Build
+    CMAKE_GENERATOR ${gen}
+    CMAKE_ARGS
+      -Dgit_EXECUTABLE:FILEPATH=${GIT_EXECUTABLE}
+      -DCMAKE_CXX_FLAGS:STRING=${CMAKE_CXX_FLAGS}
+      -DCMAKE_C_FLAGS:STRING=${CMAKE_C_FLAGS}
+      -DCMAKE_EXE_LINKER_FLAGS:STRING=${CMAKE_EXE_LINKER_FLAGS}
+      -DCMAKE_SHARED_LINKER_FLAGS:STRING=${CMAKE_SHARED_LINKER_FLAGS}
+      -DCMAKE_BUILD_TYPE:STRING=${build_type}
+      -DBUILD_SHARED_LIBS:BOOL=${shared}
+      -DBUILD_EXAMPLES:BOOL=OFF
+      -DBUILD_TESTING:BOOL=OFF
+      -DITK_USE_REVIEW:BOOL=ON
+      -DITK_USE_OPTIMIZED_REGISTRATION_METHODS:BOOL=ON
+    INSTALL_COMMAND ""
+    )
+  set( ITK_DIR "${base}/Insight-Build" )
+
+  set( CALATK_DEPENDS ${CALATK_DEPENDS} "Insight" )
+endif( NOT USE_SYSTEM_ITK )
+
+##
+## calatk - Normal Build
+##
+set( proj CALATK )
+ExternalProject_Add( ${proj}
+  DOWNLOAD_COMMAND ""
+  SOURCE_DIR "${CMAKE_CURRENT_SOURCE_DIR}"
+  BINARY_DIR CALATK-Build
+  CMAKE_GENERATOR ${gen}
+  CMAKE_ARGS
+    -DCMAKE_BUILD_TYPE:STRING=${build_type}
+    -DBUILD_SHARED_LIBS:BOOL=${shared}
+    -DBUILD_TESTING:BOOL=${testing}
+    -DBUILD_DOCUMENTATION:BOOL=${BUILD_DOCUMENTATION}
+    -DCMAKE_SHARED_LINKER_FLAGS:STRING=${CMAKE_SHARED_LINKER_FLAGS}
+    -DCMAKE_EXE_LINKER_FLAGS:STRING=${CMAKE_EXE_LINKER_FLAGS}
+    -DCMAKE_CXX_FLAGS:STRING=${CMAKE_CXX_FLAGS}
+    -DCMAKE_C_FLAGS:STRING=${CMAKE_C_FLAGS}
+    -DCALATK_USE_SUPERBUILD:BOOL=FALSE
+    -DITK_DIR:PATH=${ITK_DIR}
+  INSTALL_COMMAND ""
+  DEPENDS
+    ${CALATK_DEPENDS}
+  )
